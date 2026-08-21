8d94770c78f13425d8b0083e587d869b2431ba38
Nakul Sharma <nakulmtr25@gmail.com>
2026-08-21 19:12:47 +0530

    Add Day 12 question 2

---
+++ Day12/question2.c
@@ -0,0 +1,25 @@
+// Q24: Write a program to calculate electricity bill based on units consumed with these rates: 
+// First 100 units at ₹5/unit 
+// Next 100 units at ₹7/unit 
+// Next 100 units at ₹10/unit 
+// Above at ₹12/unit
+
+#include<stdio.h>
+int main(){
+    int bill,units;
+    printf("Enter Units : ");
+    scanf("%d",&units);
+    if(units >=0 && units <= 100 ){
+        bill = units*5;
+    }else if(units > 100 && units <= 200){
+        bill = (100*5) + ((units - 100)*7);
+        printf("%d",bill);
+    }else if(units > 200 && units <= 300){
+        bill = (100*5) +(100*7) +((units - 200)*10);
+        printf("%d",bill);
+    }else {
+        bill = (100*5) +(100*7) + (100*10)+((units - 300)*12);
+        printf("%d",bill);
+    }
+    return 0;
+}
