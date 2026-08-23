c1dca33370d72e1ddd77da515239ffac1633e2ce
Nakul Sharma <nakulmtr25@gmail.com>
2026-08-23 10:29:29 +0530

    Add Day 14 odd numbers program

---
+++ Day 14/sumofnoddnumbers.c
@@ -0,0 +1,14 @@
+// Write the program to print the sum of first n odd numbers.
+#include <stdio.h>
+int main (){
+    int n;
+    printf("Enter n : ");
+    scanf("%d",&n);
+    int sum = 0;
+    for(int i = 1;i<=n;i++){
+        sum = n*n;
+            printf("%d",sum);
+            break;
+        }
+        return 0;
+    }
