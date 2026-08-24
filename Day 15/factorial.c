73ab9f88dfe92a63436406c1bd18bcdeff943fbe
Nakul Sharma <nakulmtr25@gmail.com>
2026-08-24 12:43:38 +0530

    Add factorial Program

---
+++ Day 15/factorial.c
@@ -0,0 +1,13 @@
+// Q29: Write a program to calculate the factorial of a number.
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter Factorial Number : ");
+    scanf("%d",&n);
+    int fact = 1;
+    for(int i = 1;i<=n;i++){
+        fact *=i;
+    }
+    printf("%d",fact);
+    return 0;
+}
